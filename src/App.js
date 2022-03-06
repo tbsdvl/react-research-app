e516a6345ac853857627426446d8a241779f901a
Triston Burns <tristonburns97@gmail.com>
2022-03-06 16:46:18 -0700

    Add styled header component to app's login view

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
+import styles from "./styles/app.module.css";
+import Header from "./components/Header";
+
 
 const App = () => {
 
     return (
-        <div id="app">
-            <section id="welcome">
-                <div className="d-flex flex-column justify-content-center align-items-center p-2 text-light bg-dark">
-                    <h1>Welcome to Launchpad App!</h1>
-                    <h2>You're ready to launch, happy coding!</h2>
-                </div>
-            </section>
-            <section id="logo">
-                <div className="d-flex flex-column justify-content-center align-items-center p-2 bg-light">
-                    <img alt="logo" src="images/launchpad_logo.png" />
-                </div>
-            </section>
+        <div id="app" className={styles.app}>
+            <Header />
         </div>
     );
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,17 @@
 import React from "react";
+import styles from "../../styles/header.module.css";
+
+const Header = () => {
+
+    return (
+        <section id="app-header" className={styles.header}>
+            <div className="d-flex flex-row justify-content-around">
+                <img alt="app_logo" src="/images/Project Logo.jpg" height="100px" width="100px" />
+                <h1 className={styles.title}>App Name</h1>
+                <a className={styles.signin} href="http://localhost:3000/login">Sign In</a>
+            </div>
+        </section>
+    )
+};
+
+export default Header;
