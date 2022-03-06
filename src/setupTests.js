7cdb47ae2f4e837d51dd94e9e93dacdb4279adfd
Triston Burns <tristonburns97@gmail.com>
2022-03-06 01:11:25 -0700

    Update comments in setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,4 @@
+// These are required imports for axios requests
+// and allow jest to use jest-dom as a testing environment
 import 'regenerator-runtime/runtime';
 import "@testing-library/jest-dom";
