f3d03b8b8dc4a83d4532cd4bf8a25027e8d032db
Triston Burns <tristonburns97@gmail.com>
2022-03-11 00:14:01 -0700

    Return book results from gutenberg api

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -6,7 +6,17 @@ const Article = ({ data }) => {
 
     return (
         <Card>
-            {/* <p>{data.}</p> */}
+            <Card.Body className="d-flex flex-column justify-content-around">
+                <p>{"Title: " + data.title}</p>
+                <p>{"Authors: " + data.authors}</p>
+                <p>{"Language" + data.langauge}</p>
+                <p>{"Issued: " + data.issued}</p>
+                <p>{"Subjects: " + data.subjects}</p>
+                <p>{"Bookshelves: " + data.bookshelves}</p>
+                <p>{"LoCC" + data.locc}</p>
+                <a href={data.url} target="_blank">Find {data.title} on Gutenberg Project</a>
+                <a href={data.as_html} target="_blank">Read {data.title} as html</a>
+            </Card.Body>
         </Card>
     );
 };
--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,25 +1,43 @@
 import React from "react";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { ListGroup, ListGroupItem, Button } from "react-bootstrap";
 import Article from "../Article";
 import styles from "../../styles/home.module.css";
 
-const Articles = ({ results, setResults }) => {
+const Articles = ({ results, setResults, count, setCount }) => {
 
     return (
-        <div id="articles">
+        <div id="books">
             <h1 className={styles.signup_title}>Articles</h1>
-            <ListGroup>
+            <ListGroup className="d-flex flex-column justify-content-around">
                 {
                     results.map(article => {
 
-                        return (
-                            <ListGroupItem key={results.indexOf(article)}>
-                                <Article data={article} />
-                            </ListGroupItem>
-                        );
+                        if (results.indexOf(article) <= count) {
+                            return (
+                                <ListGroupItem className="bg-light w-50" key={results.indexOf(article)}>
+                                    <Article data={article} />
+                                </ListGroupItem>
+                            );
+                        } else {
+                            return;
+                        };
                     })
                 }
             </ListGroup>
+            {
+                results.length <= 10 ?
+                    null
+                    :
+                    <Button
+                        type="submit"
+                        onClick={() => {
+                            setCount(count + 10);
+                        }
+                        }
+                    >
+                        Load next 10 books
+                    </Button>
+            }
         </div>
     );
 };
